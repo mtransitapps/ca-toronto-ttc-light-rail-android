c4c3168e66da3362e6cd0710d4c5f4eab51b2328
Mathieu Méa <mathieu.mea@gmail.com>
2026-03-01 17:11:17 -0500

    Convert `Java` config -> `JSON`

--- agency-parser/src/main/java/org/mtransit/parser/ca_toronto_ttc_light_rail/TorontoTTCLightRailAgencyTools.java
+++ agency-parser/src/main/java/org/mtransit/parser/ca_toronto_ttc_light_rail/TorontoTTCLightRailAgencyTools.java
@@ -6,123 +6,20 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.mtransit.commons.CleanUtils;
 import org.mtransit.commons.StringUtils;
-import org.mtransit.commons.TorontoTTCCommons;
 import org.mtransit.parser.DefaultAgencyTools;
 import org.mtransit.parser.gtfs.data.GRoute;
-import org.mtransit.parser.gtfs.data.GRouteType;
 import org.mtransit.parser.gtfs.data.GStopTime;
 import org.mtransit.parser.gtfs.data.GTrip;
-import org.mtransit.parser.mt.data.MAgency;
 import org.mtransit.parser.mt.data.MDirection;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
 import java.util.regex.Pattern;
 
-// TTC Real-Time Next Vehicle Arrival (NVAS) [RETIRED]
-// - https://open.toronto.ca/dataset/ttc-real-time-next-vehicle-arrival-nvas/
-// -----
-// TTC BusTime Real-Time Next Vehicle Arrival (NVAS):
-// - https://open.toronto.ca/dataset/ttc-bustime-real-time-next-vehicle-arrival-nvas/ -> https://bustime.ttc.ca/gtfsrt
-// - Surface Routes and Schedules for BusTime: (Buses and Streetcars)
-//   - https://open.toronto.ca/dataset/surface-routes-and-schedules-for-bustime/ -> https://...opendata.inter.prod-toronto.ca/dataset/.../download/SurfaceGTFS.zip
-// -----
-// TTC GTFS-Realtime (GTFS-RT):
-// - https://open.toronto.ca/dataset/ttc-gtfs-realtime-gtfs-rt/ -> https://gtfsrt.ttc.ca/
-// - Merged GTFS - TTC Routes and Schedules:
-//   - https://open.toronto.ca/dataset/merged-gtfs-ttc-routes-and-schedules/ -> https://...opendata.inter.prod-toronto.ca/dataset/.../download/Complete%20GTFS.zip
-// -----
-// OLD: https://open.toronto.ca/dataset/ttc-routes-and-schedules/ # ALL (including SUBWAY)
-// OLD: http://opendata.toronto.ca/TTC/routes/OpenData_TTC_Schedules.zip
-// OLD: http://opendata.toronto.ca/toronto.transit.commission/ttc-routes-and-schedules/OpenData_TTC_Schedules.zip
 public class TorontoTTCLightRailAgencyTools extends DefaultAgencyTools {
 
 	public static void main(@NotNull String[] args) {
 		new TorontoTTCLightRailAgencyTools().start(args);
-	}
-
-	@Nullable
-	@Override
-	public List<Locale> getSupportedLanguages() {
-		return LANG_EN;
-	}
-
-	private static final Pattern NOT_IN_SERVICE_ = Pattern.compile("(Not In Service)", Pattern.CASE_INSENSITIVE);
-
-	@Override
-	public boolean excludeTrip(@NotNull GTrip gTrip) {
-		if (NOT_IN_SERVICE_.matcher(gTrip.getTripHeadsignOrDefault()).matches()) {
-			return EXCLUDE;
-		}
-		return super.excludeTrip(gTrip);
-	}
-
-	@Override
-	public boolean excludeStopTime(@NotNull GStopTime gStopTime) {
-		if (NOT_IN_SERVICE_.matcher(gStopTime.getStopHeadsignOrDefault()).matches()) {
-			return EXCLUDE;
-		}
-		return super.excludeStopTime(gStopTime);
-	}
-
-	@NotNull
-	@Override
-	public String getAgencyName() {
-		return "TTC";
-	}
-
-	@Override
-	public @NotNull Integer getAgencyRouteType() {
-		return MAgency.ROUTE_TYPE_LIGHT_RAIL;
-	}
-
-	@NotNull
-	@Override
-	public Integer getAgencyExtendedRouteType() {
-		return GRouteType.EX_TRAM_SERVICE.getId();
-	}
-
-	@Override
-	public boolean defaultRouteIdEnabled() {
-		return true;
-	}
-
-	@Override
-	public boolean useRouteShortNameForRouteId() {
-		return true;
-	}
-
-	@Override
-	public boolean defaultRouteLongNameEnabled() {
-		return true;
-	}
-
-	@NotNull
-	@Override
-	public String cleanRouteLongName(@NotNull String routeLongName) {
-		routeLongName = CleanUtils.toLowerCaseUpperCaseWords(getFirstLanguageNN(), routeLongName);
-		return CleanUtils.cleanLabel(getFirstLanguageNN(), routeLongName);
-	}
-
-	@Override
-	public @NotNull String getAgencyColor() {
-		return TorontoTTCCommons.TTC_RED;
-	}
-
-	@Override
-	public boolean defaultAgencyColorEnabled() {
-		return true;
-	}
-
-	@Nullable
-	@Override
-	public String fixColor(@Nullable String color) {
-		final String fixedColor = TorontoTTCCommons.fixColor(color);
-		if (fixedColor != null) {
-			return fixedColor;
-		}
-		return super.fixColor(color);
 	}
 
 	@NotNull
@@ -132,11 +29,6 @@ public class TorontoTTCLightRailAgencyTools extends DefaultAgencyTools {
 				MDirection.HEADSIGN_TYPE_DIRECTION,
 				MDirection.HEADSIGN_TYPE_STRING // route 5
 		);
-	}
-
-	@Override
-	public boolean directionFinderEnabled() {
-		return true;
 	}
 
 	private static final String L_ = "L ";
@@ -177,7 +69,7 @@ public class TorontoTTCLightRailAgencyTools extends DefaultAgencyTools {
 	@Override
 	public @NotNull String cleanDirectionHeadsign(@Nullable GRoute gRoute, int directionId, boolean fromStopName, @NotNull String directionHeadSign) {
 		directionHeadSign = STARTS_WITH_DASH_.matcher(directionHeadSign).replaceAll(EMPTY); // keep East/West/North/South
-		directionHeadSign = CleanUtils.toLowerCaseUpperCaseWords(Locale.ENGLISH, directionHeadSign);
+		directionHeadSign = CleanUtils.toLowerCaseUpperCaseWords(getFirstLanguageNN(), directionHeadSign);
 		directionHeadSign = CleanUtils.keepTo(directionHeadSign); // LRT 5 & 6
 		return super.cleanDirectionHeadsign(gRoute, directionId, fromStopName, directionHeadSign);
 	}
@@ -208,7 +100,7 @@ public class TorontoTTCLightRailAgencyTools extends DefaultAgencyTools {
 		tripHeadsign = SHORT_TURN_.matcher(tripHeadsign).replaceAll(EMPTY);
 		tripHeadsign = BLUE_NIGHT_.matcher(tripHeadsign).replaceAll(EMPTY);
 		tripHeadsign = CleanUtils.removeVia(tripHeadsign);
-		tripHeadsign = CleanUtils.toLowerCaseUpperCaseWords(Locale.ENGLISH, tripHeadsign);
+		tripHeadsign = CleanUtils.toLowerCaseUpperCaseWords(getFirstLanguageNN(), tripHeadsign);
 		tripHeadsign = CleanUtils.fixMcXCase(tripHeadsign);
 		tripHeadsign = CleanUtils.CLEAN_AT.matcher(tripHeadsign).replaceAll(CleanUtils.CLEAN_AT_REPLACEMENT);
 		tripHeadsign = CleanUtils.CLEAN_AND.matcher(tripHeadsign).replaceAll(CleanUtils.CLEAN_AND_REPLACEMENT);
@@ -235,23 +127,5 @@ public class TorontoTTCLightRailAgencyTools extends DefaultAgencyTools {
 				.matcher(stopHeadsign).replaceAll(RSN_RLN_REPLACEMENT);
 		return super.cleanStopHeadSign(gRoute, gTrip, gStopTime, stopHeadsign);
 	}
-
-	private static final Pattern SIDE = Pattern.compile("((^|\\W)(side)(\\W|$))", Pattern.CASE_INSENSITIVE);
-	private static final String SIDE_REPLACEMENT = "$2" + "$4";
-
-	private static final Pattern ENDS_WITH_TOWARDS_ = Pattern.compile("( towards .*$)", Pattern.CASE_INSENSITIVE);
-
-	@NotNull
-	@Override
-	public String cleanStopName(@NotNull String gStopName) {
-		gStopName = ENDS_WITH_TOWARDS_.matcher(gStopName).replaceAll(EMPTY);
-		gStopName = CleanUtils.toLowerCaseUpperCaseWords(Locale.ENGLISH, gStopName);
-		gStopName = CleanUtils.CLEAN_AT.matcher(gStopName).replaceAll(CleanUtils.CLEAN_AT_REPLACEMENT);
-		gStopName = SIDE.matcher(gStopName).replaceAll(SIDE_REPLACEMENT);
-		gStopName = CleanUtils.cleanBounds(gStopName);
-		gStopName = CleanUtils.fixMcXCase(gStopName);
-		gStopName = CleanUtils.cleanStreetTypes(gStopName);
-		gStopName = CleanUtils.cleanNumbers(gStopName);
-		return CleanUtils.cleanLabel(getFirstLanguageNN(), gStopName);
-	}
 }
+
